384ec7219700b6c2f7ed0786d20fc1fccd4f2276
Konrad Sikorski <konrad.sikorski@windowslive.com>
2017-04-12 23:43:36 +0200

    initial checkin for Survival Shooter

---
+++ SurvivalShooter/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 6f;
+    private Vector3 movement;
+    private Animator anim;
+    private Rigidbody playerRigidbody;
+    private int floorMask;
+    float camRayLength = 100f;
+
+    // like start method but this one is called regardles the script is enabled or not
+    void Awake()
+    {
+        floorMask = LayerMask.GetMask("Floor");
+        anim = GetComponent<Animator>();
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        // w odroznieniu od GetAxis() ktory zwraca wartosc od -1 do 1 (float), GetAxisRaw zwraca jedna z trzech wartosci -1,0,1
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        Move(h, v);
+        Turn();
+        Animating(h, v);
+    }
+
+    private void Move(float h, float v)
+    {
+        movement.Set(h, 0, v);
+        movement = movement.normalized * speed * Time.deltaTime;
+        playerRigidbody.MovePosition(transform.position + movement);
+    }
+
+    private void Turn()
+    {
+        Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit floorHit;
+        if(Physics.Raycast(camRay, out floorHit, camRayLength, floorMask))
+        {
+            Vector3 playerToMouse = floorHit.point - transform.position;
+            playerToMouse.y = 0;
+
+            Quaternion newRotation = Quaternion.LookRotation(playerToMouse);
+            playerRigidbody.MoveRotation(newRotation);
+        }
+    }
+
+    private void Animating(float h, float v)
+    {
+        bool walking = h != 0 || v != 0;
+        anim.SetBool("IsWalking", walking);
+    }
+}
