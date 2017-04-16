775a102a8852b67e2447598a3781fe25098c7962
Konrad Sikorski <konrad.sikorski@windowslive.com>
2017-04-16 14:16:35 +0200

    shooting

---
+++ SpaceShooter/Assets/Scripts/BoltController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoltController : MonoBehaviour {
+    public float Speed;
+    public float zMax;
+    private Rigidbody boltBody;
+
+	// Use this for initialization
+	void Start () {
+        boltBody = GetComponent<Rigidbody>();
+        boltBody.velocity = transform.forward * Speed;
+	}
+
+    private void Update()
+    {
+        if (transform.position.z >= zMax)
+            Destroy(gameObject);
+    }
+}
---
+++ SpaceShooter/Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour {
+    public bool IsAutoFire;
+    public float FireSpeed;
+    public GameObject Bolt;
+
+    private Transform shootSpown;
+    //private float fireDeltaTime;
+    private float nextFireTime;
+
+	// Use this for initialization
+	void Start () {
+        shootSpown = transform.FindChild("ShootSpown");
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+            IsAutoFire = !IsAutoFire;
+
+        // first apouch
+        //if (IsFireing() && fireDeltaTime >= FireSpeed)
+        //    Fire();
+        //else
+        //    fireDeltaTime += Time.deltaTime;
+
+        // seccond aprouch
+        if (IsFireing() && Time.time > nextFireTime)
+        {
+            nextFireTime = Time.time + FireSpeed;
+            Fire();
+        }
+    }
+
+    private bool IsFireing()
+    {
+        return IsAutoFire || Input.GetButton("Fire1");
+    }
+
+    private void Fire()
+    {
+        Instantiate(Bolt, shootSpown.position, shootSpown.rotation);
+    }
+}
